b4c104bd1565390b82e0ec0140baf96744d97b92
369997 <369997@gmail.com>
2020-05-07 01:00:33 +0300

    add serializer

---
+++ README.md
@@ -0,0 +1,2 @@
+> pipenv install django djangorestframework django-rest-knox
+> Ctrl+shift+P python select interpreter (the folder name env)
---
+++ leadmanager/leads/api.py
@@ -0,0 +1,5 @@
+from leads.models import Lead
+from rest_framework import viewsets, permissions
+from .serializers import LeadSerializer
+
+# Lead Viewset
